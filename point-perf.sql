ce8bbe92ea8553b7aa5f0b4065f89edefd13d89c
Martin Davis <mtnclimb@gmail.com>
2019-10-04 17:19:02 -0700

    Update point-perf.sql

--- point-perf.sql
+++ point-perf.sql
@@ -8,5 +8,14 @@ CREATE TABLE pts.pts_xy
   locy double precision
 );
 
+DELETE FROM pts.pts_xy;
 
+INSERT INTO pts.pts_xy 
+  SELECT 100*random() AS x, 100*random() AS y
+    FROM generate_series(1, 1000000) AS t(x);
 
+SELECT count(*) FROM pts.pts_xy 
+  WHERE locx BETWEEN 50 AND 51 AND locy BETWEEN 60 AND 61;
+  
+  -- Clean up
+  

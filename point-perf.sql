8e34763114e2ade3593642230e418460e923e3d3
Martin Davis <mtnclimb@gmail.com>
2019-10-04 17:24:31 -0700

    Update point-perf.sql

--- point-perf.sql
+++ point-perf.sql
@@ -8,8 +8,11 @@ CREATE TABLE pts.pts_xy
   locy double precision
 );
 
+CREATE INDEX ON pts.pts_xy ( locx, locy );
+
 DELETE FROM pts.pts_xy;
 
+-- Insert 1M records with location ordinates randomly in [0,100]
 INSERT INTO pts.pts_xy 
   SELECT 100*random() AS x, 100*random() AS y
     FROM generate_series(1, 1000000) AS t(x);
@@ -17,5 +20,7 @@ INSERT INTO pts.pts_xy
 SELECT count(*) FROM pts.pts_xy 
   WHERE locx BETWEEN 50 AND 51 AND locy BETWEEN 60 AND 61;
   
-  -- Clean up
+-- Test Results:
+-- No index: 184 ms
+-- Index: 4.65 ms
   

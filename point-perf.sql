399382d3fcea9f22596bf938ca95c901c7d2eef0
Martin Davis <mtnclimb@gmail.com>
2019-10-05 13:39:18 -0700

    Update point-perf.sql

--- point-perf.sql
+++ point-perf.sql
@@ -1,26 +1,24 @@
 -- Performance tests for point data and indexes
 
-CREATE SCHEMA pts;
+CREATE SCHEMA ptsperf;
 
 -- ================================================
 -- Representation: X,Y columns, with BTREE index
 -- ================================================
-CREATE TABLE pts.pts_xy
+CREATE TABLE ptsperf.pts_xy
 (
   locx double precision,
   locy double precision
 );
 
-CREATE INDEX ON pts.pts_xy ( locx, locy );
+CREATE INDEX ON ptsperf.pts_xy ( locx, locy );
 
-DELETE FROM pts.pts_xy;
-
--- Insert 1M records with location ordinates random in [0,100]
-INSERT INTO pts.pts_xy 
+-- Insert records with location ordinates random in [0,100]
+INSERT INTO ptsperf.pts_xy 
   SELECT 100*random() AS x, 100*random() AS y
-    FROM generate_series(1, 1000000) AS t(x);
+    FROM generate_series(1, 10000000) AS t(x);
 
-SELECT count(*) FROM pts.pts_xy 
+SELECT count(*) FROM ptsperf.pts_xy 
   WHERE locx BETWEEN 50 AND 51 AND locy BETWEEN 60 AND 61;
   
 -- Test Results:
@@ -28,25 +26,31 @@ SELECT count(*) FROM pts.pts_xy
 -- Index: 4.65 ms
   
 -- ================================================
--- Representation: point, with GIST index
+-- Representation: Postgres point, with GIST index
 -- ================================================
-CREATE TABLE pts.pts_pt
+CREATE TABLE ptsperf.pts_point
 (
   loc point
 );
 
-CREATE INDEX ON pts.pts_pt USING gist( loc );
+CREATE INDEX ON ptsperf.pts_point USING gist( loc );
 
-DELETE FROM pts.pts_pt;
+DELETE FROM pts.pts_point;
 
--- Insert 1M records with location ordinates random in [0,100]
-INSERT INTO pts.pts_pt 
-  SELECT point('(' || 100*random() || ',' || 100*random() || ')') AS loc
-    FROM generate_series(1, 1000000) AS t(x);
+-- Insert records with locations as point datatype 
+INSERT INTO ptsperf.pts_point 
+  SELECT point('(' || locx || ',' || locy || ')') AS loc
+    FROM ptsperf.pts_xy;
 
-SELECT count(*) FROM pts.pts_pt 
+SELECT count(*) FROM pts.pts_point 
   WHERE loc <@ box '((50,60),(51,61))';
 
 -- Test Results:
 -- No index: 110 ms
 -- Index: 1.2 - 2.4 ms
+
+-- ===================================
+-- Cleanup
+-- ===================================
+
+DROP SCHEMA ptsperf CASCADE;

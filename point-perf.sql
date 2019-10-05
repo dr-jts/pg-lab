c897d0693bbeab6d24d6826dd6f93b922c66bb53
Martin Davis <mtnclimb@gmail.com>
2019-10-04 20:09:29 -0700

    Update point-perf.sql

--- point-perf.sql
+++ point-perf.sql
@@ -2,6 +2,9 @@
 
 CREATE SCHEMA pts;
 
+-- ================================================
+-- Representation: X,Y columns, with BTREE index
+-- ================================================
 CREATE TABLE pts.pts_xy
 (
   locx double precision,
@@ -12,7 +15,7 @@ CREATE INDEX ON pts.pts_xy ( locx, locy );
 
 DELETE FROM pts.pts_xy;
 
--- Insert 1M records with location ordinates randomly in [0,100]
+-- Insert 1M records with location ordinates random in [0,100]
 INSERT INTO pts.pts_xy 
   SELECT 100*random() AS x, 100*random() AS y
     FROM generate_series(1, 1000000) AS t(x);
@@ -24,6 +27,9 @@ SELECT count(*) FROM pts.pts_xy
 -- No index: 184 ms
 -- Index: 4.65 ms
   
+-- ================================================
+-- Representation: point, with GIST index
+-- ================================================
 CREATE TABLE pts.pts_pt
 (
   loc point
@@ -33,10 +39,14 @@ CREATE INDEX ON pts.pts_pt USING gist( loc );
 
 DELETE FROM pts.pts_pt;
 
--- Insert 1M records with location ordinates randomly in [0,100]
+-- Insert 1M records with location ordinates random in [0,100]
 INSERT INTO pts.pts_pt 
   SELECT point('(' || 100*random() || ',' || 100*random() || ')') AS loc
     FROM generate_series(1, 1000000) AS t(x);
 
-SELECT count(*) FROM pts.pts_xy 
-  WHERE locx BETWEEN 50 AND 51 AND locy BETWEEN 60 AND 61;
+SELECT count(*) FROM pts.pts_pt 
+  WHERE loc <@ box '((50,60),(51,61))';
+
+-- Test Results:
+-- No index: 110 ms
+-- Index: 1.2 - 2.4 ms
